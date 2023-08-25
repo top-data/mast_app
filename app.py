01e0a438c6ba6d137d2a1d94bf5be263bf7fc130
Mahdi <85419370+top-data@users.noreply.github.com>
2023-08-25 11:49:08 +1000

    Update app.py

--- app.py
+++ app.py
@@ -26,4 +26,4 @@ def upload_file():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
